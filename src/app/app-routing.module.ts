533438c0158e5007fc0f7473c84ba1663a76c564
MMS <35738176+SK68-ph@users.noreply.github.com>
2022-11-13 22:42:25 +0800

    fixed home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ContactComponent } from './contact/contact.component';
 // const routes: Routes = [];
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'foods', component: FoodsComponent },
   { path: 'contact', component: ContactComponent },
 ];
